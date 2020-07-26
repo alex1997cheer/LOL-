9a0cdbcaf64650049f7c1ce0596f08814f45ebba
Frank <frankhelpful@163.com>
2020-07-26 21:11:39 +0800

    First version

---
+++ SelectionHero/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SelectionHero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "英雄名：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "英雄锁定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "亚索",
+            "卡牌大师"});
+            this.comboBox1.Location = new System.Drawing.Point(71, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(145, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(31, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 36);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 112);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SelectionHero/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dm;
+namespace SelectionHero
+{
+
+    public partial class Form1 : Form 
+    {
+        dmsoft DM;
+        Dictionary<string, List<int>> Hero;
+        //Hero
+        public Form1()
+        {
+            Hero = new Dictionary<string, List<int>>() {
+                { "亚索", new List<int>(){ (int)Keys.Y, (int)Keys.A, (int)Keys.S, (int)Keys.U, (int)Keys.O, (int)Keys.Space }},
+                { "卡牌大师", new List<int>(){ (int)Keys.K, (int)Keys.A, (int)Keys.P, (int)Keys.A, (int)Keys.I, (int)Keys.D, (int)Keys.A, (int)Keys.S, (int)Keys.H, (int)Keys.I, (int)Keys.Space}}
+            };
+            DM = new dmsoft();
+            DM.SetPath(@"C:\Users\Administrator\Desktop\AutomationSelectHere\Image");
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            string HeroName = comboBox1.Text.ToString();
+            var LoLClient = DM.FindWindow("", "League of Legends");            
+            DM.BindWindow(LoLClient, "gdi", "windows3", "normal", 1);
+            DM.MoveTo(860, 97);
+            DM.LeftClick();
+            DM.Capture(0, 0, 2000, 2000, "1.bmp");
+            int isExsit = -1;
+            //Enter hero name X Y;
+            int x=784, y=100;
+            object x1, y2;
+            while(true)
+            {
+                isExsit = DM.FindPic(0, 0, 2000, 2000, "Icon.bmp", "000000", 0.9, 0, out x1, out y2);
+                if (isExsit >= 0)
+                {
+                    DM.MoveTo(860, 97);
+                    Thread.Sleep(300);
+                    DM.LeftClick();
+                    Thread.Sleep(300);
+                    foreach (int key in Hero[HeroName])
+                    {
+                        DM.KeyPress(key);
+                    }                    
+                    DM.MoveTo(370, 163);
+                    Thread.Sleep(500);
+                    DM.LeftClick();
+                }
+            }
+        }
+    }
+}
